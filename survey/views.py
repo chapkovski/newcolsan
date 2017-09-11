aeff1a009b9eded0666ca2aceaefb99338956cd7
chapkovski <chapkovski@gmail.com>
2017-09-11 19:58:25 +0200

    adding survey

---
+++ survey/views.py
@@ -0,0 +1,17 @@
+from otree.api import Currency as c, currency_range
+from . import models
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+
+class Survey(Page):
+    form_model = models.Player
+    form_fields = ['comment']
+
+
+
+
+page_sequence = [
+    Survey,
+
+]
